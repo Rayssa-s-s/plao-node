f55acbcd3566e37de968a872e43f1c2ba4dd6d62
Rayssa-s-s <85031056+Rayssa-s-s@users.noreply.github.com>
2021-06-26 15:03:44 -0300

    Add files via upload

--- app/Controllers/home.js
+++ app/Controllers/home.js
@@ -1,3 +1,7 @@
 module.exports.index = (application, req, res)=>{
-    res.render("home/index");
+    const connection = application.config.dbConnection;
+    const noticiasDAO = new application.app.models.NoticiasDAO(connection);
+    noticiasDAO.get5Ultimasnoticias((error, result)=>{
+        res.render("noticias/noticias", {noticias: result })
+   });
 }
--- app/Controllers/noticias.js
+++ app/Controllers/noticias.js
@@ -3,13 +3,13 @@ module.exports.noticias = (application, req, res)=>{
     const noticiasDAO = new application.app.models.NoticiasDAO(connection);
     noticiasDAO.getNoticias((error, result)=>{
         res.render("noticias/noticias", {noticias: result })
-   })
+   });
 }
 
 module.exports.noticia = (application, req, res)=>{
-    const connection = application.config.dbConnection();
+    const connection = application.config.dbConnection;
     const noticiasDAO = new application.app.models.NoticiasDAO(connection);
-    noticiasDAO.getNoticias((error, result)=>{
-        res.render("noticias/noticia", {noticia: result })
-   })
+    noticiasDAO.getNoticias((error, result)=> {
+        res.render("noticias/noticia", {noticia: result });
+   });
 }
